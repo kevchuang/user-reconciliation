0bce385239077b0e1760a0cfc272a1a796b2c00a
kevin.chuang <kchuang.pro@gmail.com>
2023-04-03 21:21:18 +0200

    Add commentary in RequestBuffer object

--- src/main/scala/com/contentsquare/service/buffer/RequestBuffer.scala
+++ src/main/scala/com/contentsquare/service/buffer/RequestBuffer.scala
@@ -12,10 +12,21 @@ object RequestBuffer {
   }
 
   abstract class LiveService(buffer: Ref[Int], limit: Int) extends Service {
+
+    /**
+     * Returns an effect that will add one into the buffer and get the count.
+     */
     override def add: UIO[Int] = buffer.updateAndGet(_ + 1)
 
+    /**
+     * Returns an effect that will produce a boolean. If the buffer has reached
+     * its limit, it produces true and false if it hasn't.
+     */
     override def hasReachedLimit: UIO[Boolean] = buffer.get.map(_ >= limit)
 
+    /**
+     * Returns an effect that will remove one from the buffer and get the count.
+     */
     override def remove: UIO[Int] = buffer.updateAndGet(_ - 1)
   }
 }
